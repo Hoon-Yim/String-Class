de3171d3914eae6104af36d0f4970bd73990e753
Hoon-Yim <yimsh4507@gmail.com>
2020-09-28 13:48:07 -0400

    first commit

---
+++ MyString.cpp
@@ -0,0 +1,6 @@
+/*
+ * File Name: MyString.cpp
+ * Last Update: [Sep, 28, 2020]
+ */
+
+#include "MyString.h"
---
+++ MyString.h
@@ -0,0 +1,11 @@
+/*
+ * File Name: MyString.h
+ * Last Update: [Sep, 28, 2020]
+ */
+
+#ifndef MY_OWN_STRING_MYSTRING_H
+#define MY_OWN_STRING_MYSTRING_H
+
+
+
+#endif //MY_OWN_STRING_MYSTRING_H
